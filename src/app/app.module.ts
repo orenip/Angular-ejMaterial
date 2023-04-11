26a6c21053e421a336405582eab24cae9c2f5c0b
Jose Antonio López Piñero <josanlopi@gmail.com>
2023-04-11 10:26:09 +0200

    10-4-23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //Importamos el modulo personalizado de Anguilar material
+    MaterialModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
